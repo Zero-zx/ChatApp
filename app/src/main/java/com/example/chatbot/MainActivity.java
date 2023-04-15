e176a85554905292852778349568d31eedb90f71
Zero-zx <dthieu2512@gmail.com>
2023-04-15 10:55:09 +0700

    first commit

---
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -0,0 +1,106 @@
+
+	 
+	/*
+     *	This content is generated from the API File Info.
+     *	(Alt+Shift+Ctrl+I).
+     *
+     *	@desc
+     *	@file 		002
+     *	@date 		Saturday 15th of April 2023 01:42:21 AM
+     *	@title 		content
+     *	@author
+     *	@keywords
+     *	@generator 	Export Kit v1.3.figma
+     *
+     */
+
+
+    package com.example.chatbot;
+
+    import android.app.Activity;
+    import android.os.Bundle;
+
+
+    import android.view.View;
+    import android.widget.TextView;
+    import android.widget.ImageView;
+
+    public class MainActivity extends Activity {
+
+
+    private View _bg___002_ek2;
+    private View rectangle;
+    private TextView write;
+    private View rectangle_ek1;
+    private ImageView subtract;
+    private View rectangle_ek2;
+    private ImageView union;
+    private View rectangle_ek3;
+    private ImageView vector;
+    private ImageView vector_ek1;
+    private View rectangle_ek4;
+    private TextView maciej_kowalski_email_com;
+    private TextView _08_43;
+    private View rectangle_ek5;
+    private TextView __;
+    private View rectangle_ek6;
+    private TextView i_am_almost_finish__please_give_me_your_email__i_will_zip_them_and_send_you_as_son_as_im_finish_;
+    private TextView _08_12;
+    private View rectangle_ek7;
+    private TextView next_month_;
+    private View rectangle_ek8;
+    private TextView i_am_in_a_process_of_designing_some__when_do_you_need_them_;
+    private View rectangle_ek9;
+    private TextView i_commented_on_figma__i_want_to_add_some_fancy_icons__do_you_have_any_icon_set_;
+    private TextView _1_feb_12_00;
+    private TextView maciej_kowalski;
+    private TextView martina_wolna;
+    private ImageView image;
+    private View rectangle_ek10;
+    private ImageView vector_ek2;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+            _bg___002_ek2 = (View) findViewById(R.id._bg___002_ek2);
+            rectangle = (View) findViewById(R.id.rectangle);
+            write = (TextView) findViewById(R.id.write);
+            rectangle_ek1 = (View) findViewById(R.id.rectangle_ek1);
+            subtract = (ImageView) findViewById(R.id.subtract);
+            rectangle_ek2 = (View) findViewById(R.id.rectangle_ek2);
+            union = (ImageView) findViewById(R.id.union);
+            rectangle_ek3 = (View) findViewById(R.id.rectangle_ek3);
+            vector = (ImageView) findViewById(R.id.vector);
+            vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+            rectangle_ek4 = (View) findViewById(R.id.rectangle_ek4);
+            maciej_kowalski_email_com = (TextView) findViewById(R.id.maciej_kowalski_email_com);
+            _08_43 = (TextView) findViewById(R.id._08_43);
+            rectangle_ek5 = (View) findViewById(R.id.rectangle_ek5);
+            __ = (TextView) findViewById(R.id.__);
+            rectangle_ek6 = (View) findViewById(R.id.rectangle_ek6);
+            i_am_almost_finish__please_give_me_your_email__i_will_zip_them_and_send_you_as_son_as_im_finish_ = (TextView) findViewById(R.id.i_am_almost_finish__please_give_me_your_email__i_will_zip_them_and_send_you_as_son_as_im_finish_);
+            _08_12 = (TextView) findViewById(R.id._08_12);
+            rectangle_ek7 = (View) findViewById(R.id.rectangle_ek7);
+            next_month_ = (TextView) findViewById(R.id.next_month_);
+            rectangle_ek8 = (View) findViewById(R.id.rectangle_ek8);
+            i_am_in_a_process_of_designing_some__when_do_you_need_them_ = (TextView) findViewById(R.id.i_am_in_a_process_of_designing_some__when_do_you_need_them_);
+            rectangle_ek9 = (View) findViewById(R.id.rectangle_ek9);
+            i_commented_on_figma__i_want_to_add_some_fancy_icons__do_you_have_any_icon_set_ = (TextView) findViewById(R.id.i_commented_on_figma__i_want_to_add_some_fancy_icons__do_you_have_any_icon_set_);
+            _1_feb_12_00 = (TextView) findViewById(R.id._1_feb_12_00);
+            maciej_kowalski = (TextView) findViewById(R.id.maciej_kowalski);
+            martina_wolna = (TextView) findViewById(R.id.martina_wolna);
+            image = (ImageView) findViewById(R.id.image);
+            rectangle_ek10 = (View) findViewById(R.id.rectangle_ek10);
+            vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+
+
+            //custom code goes here
+
+            }
+            }
+	
+	
